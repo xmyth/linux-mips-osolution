74c7d75de8fed6ce527b4739f2c8e862b0aebd18
swadmin.os <swadmin.os@4d105e58-db3a-0410-9e81-b9488bac83f7>
2008-01-07 08:41:52 +0000

    add the battery info support

--- drivers/loongson/apm_emu.c
+++ drivers/loongson/apm_emu.c
@@ -6,32 +6,131 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/apm-emulation.h>
+#include <linux/i2c.h>
 
 static void ls_apm_get_power_status(struct apm_power_info *info)
 {
-	int percentage = -1;
-	int batteries = 0;
-	int time_units = -1;
-	int real_count = 0;
-	int i;
-	char charging = 0;
-	long charge = -1;
-	long amperage = 0;
-	unsigned long btype = 0;
+	struct i2c_adapter *p;
+	union i2c_smbus_data data, data1, data2, data3;
 
-	info->battery_status = APM_BATTERY_STATUS_UNKNOWN;
+ 	//data batter mode; data1 remain capacity; data2 full capacity; data3 charge/discharge current
+
 	info->battery_flag = APM_BATTERY_FLAG_UNKNOWN;
 	info->units = APM_UNITS_MINS;
-	info->ac_line_status = APM_AC_ONLINE;
 
-	info->battery_life = 100;
-	info->time = 20;
+	p = i2c_get_adapter(0);
+
+	if (p == NULL)
+	{
+		printk(KERN_ERR "i2c adapter not found! \n");
+	}
+
+	//Charge Current
+	if (!i2c_smbus_xfer(p, 0xb, 0, I2C_SMBUS_READ, 0x16, I2C_SMBUS_WORD_DATA,&data)
+		&& !i2c_smbus_xfer(p, 0xb, 0, I2C_SMBUS_READ, 0xa, I2C_SMBUS_WORD_DATA,&data3))
+		;
+	else
+	{
+		//Battery not existed.
+		info->ac_line_status = APM_AC_ONLINE;
+		info->battery_status = APM_BATTERY_STATUS_UNKNOWN;
+		info->battery_flag = APM_BATTERY_FLAG_UNKNOWN;
+		info->units = APM_UNITS_MINS;
+		info->battery_life = 0;
+		info->time = 0;
+		return;	
+	}
+
+	if (data.word & 0x0040 && data3.word != 0)
+	{
+		info->ac_line_status = APM_AC_OFFLINE;
+
+		if (data.word & 0x0200)
+		{
+			info->battery_status = APM_BATTERY_STATUS_LOW;
+		}
+		if (!i2c_smbus_xfer(p, 0xb, 0, I2C_SMBUS_READ, 0x0f, I2C_SMBUS_WORD_DATA,&data1))
+		{
+			//printk(KERN_INFO "apm_emu: 0xf %d\n", data1.word);
+			if (!i2c_smbus_xfer(p, 0xb, 0, I2C_SMBUS_READ, 0x10, I2C_SMBUS_WORD_DATA,&data2))
+			{
+				//printk(KERN_INFO "apm_emu: 0x10 %d\n", data2.word);
+				info->battery_life = data1.word * 100 / data2.word;
+		
+				if (!i2c_smbus_xfer(p, 0xb, 0, I2C_SMBUS_READ, 0x11, I2C_SMBUS_WORD_DATA,&data3))
+				{
+					//printk(KERN_INFO "apm_emu: 11 %d\n", data3.word);
+					info->time = data3.word;
+				}
+				else
+				{
+					info->time = 0xffff;
+				}
+			}
+			else
+			{
+				info->battery_life = 0;
+				info->time = 0;
+			}
+		}
+		else
+		{
+			info->battery_life = 0;
+			info->time = 0;
+		}
+	}
+	else
+	{
+		info->battery_status = APM_AC_ONLINE;
+		if (data.word & 0x0020)
+		{
+			info->battery_status = APM_BATTERY_STATUS_HIGH;
+			info->battery_life = 100;
+		}
+		else
+		{
+			info->battery_status = APM_BATTERY_STATUS_CHARGING;
+			if (!i2c_smbus_xfer(p, 0xb, 0, I2C_SMBUS_READ, 0x0f, I2C_SMBUS_WORD_DATA,&data1))
+			{
+				//printk(KERN_INFO "apm_emu: 0xf %d\n", data1.word);
+				if (!i2c_smbus_xfer(p, 0xb, 0, I2C_SMBUS_READ, 0x10, I2C_SMBUS_WORD_DATA,&data2))
+				{
+					//printk(KERN_INFO "apm_emu: 0x10 %d\n", data2.word);
+					info->battery_life = data1.word * 100 / data2.word;
+
+					if (!i2c_smbus_xfer(p, 0xb, 0, I2C_SMBUS_READ, 0x13, I2C_SMBUS_WORD_DATA,&data3))
+					{
+						//printk(KERN_INFO "apm_emu: 13 %d\n", data3.word);
+						info->time = data3.word;
+					}
+					else
+					{
+						info->time = 0xffff;
+					}
+				}
+				else
+				{
+					info->battery_life = 0;
+					info->time = 0;
+				}
+			}
+			else
+			{
+				info->battery_life = 0;
+				info->time = 0;
+			}
+			
+		}
+	}
+
+	
 }
 
 static int __init apm_emu_init(void)
 {
-	apm_get_power_status = ls_apm_get_power_status;
 
+	apm_get_power_status = ls_apm_get_power_status;	
+ 
 	printk(KERN_INFO "apm_emu: Loonson APM Emulation initialized.\n");
 
 	return 0;
