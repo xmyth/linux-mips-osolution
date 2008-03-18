34a4b0c1d7250ecea80255029384099b0bcfc79b
swadmin.os <swadmin.os@4d105e58-db3a-0410-9e81-b9488bac83f7>
2008-03-18 13:33:17 +0000

    update build script for adding xgi option 	run 	./release.sh xgi 	for building xgi as video driver

--- trunk/release.sh
+++ trunk/release.sh
@@ -1,3 +1,19 @@
 #/bin/bash
-make mrproper && make ls2fdev_defconfig && make && rm -fr rel && mkdir rel && INSTALL_MOD_PATH=rel make modules_install && mkdir rel/boot && cp vmlinux rel/boot/ && cd rel && tar -cjf linux-2.6.23.1-ls2f.tar.bz2 boot lib
+echo
+
+CONFIG_NAME=ls2fdev
+if [ "$#" -eq 1 ] && [ "$1" == "xgi" ];then
+	echo "Build xgifb as video driver"
+	CONFIG_NAME=ls2fdev_xgi
+else
+	echo "Build sisfb as video driver"
+fi
+echo
+echo "Get the Revision number from svn"
+Revision=`svn info|grep Revision:`
+REV_NUM=${Revision##* }
+echo "Revision:$REV_NUM"
+echo
+
+make mrproper && make ${CONFIG_NAME}_defconfig && make && rm -fr rel && mkdir rel && INSTALL_MOD_PATH=rel make modules_install && mkdir rel/boot && cp vmlinux rel/boot/ && cd rel && tar -cjf ../linux-2.6.23.1-r$REV_NUM-${CONFIG_NAME}.tar.bz2 boot lib
 
