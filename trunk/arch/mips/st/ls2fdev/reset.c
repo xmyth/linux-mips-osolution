484226174883697db6b8606300991276ee45415f
swadmin.os <swadmin.os@4d105e58-db3a-0410-9e81-b9488bac83f7>
2008-04-20 12:32:09 +0000

    Add ST7 Software PowerOff Support

--- trunk/arch/mips/st/ls2fdev/reset.c
+++ trunk/arch/mips/st/ls2fdev/reset.c
@@ -11,6 +11,8 @@
 
 #include <asm/reboot.h>
 
+#include "via686b.h"  //This file comes from pmon definition
+
 static void loongson2e_restart(char *command)
 {
 #ifdef CONFIG_32BIT
@@ -23,8 +25,11 @@ static void loongson2e_restart(char *command)
 	__asm__ __volatile__("jr\t%0"::"r"(0xbfc00000));
 }
 
+static void loongson2f_soft_off(void);
+
 static void loongson2e_halt(void)
 {
+	loongson2f_soft_off();
 	while (1) ;
 }
 
@@ -38,4 +43,57 @@ void mips_reboot_setup(void)
 	_machine_restart = loongson2e_restart;
 	_machine_halt = loongson2e_halt;
 	pm_power_off = loongson2e_power_off;
+}
+
+static void loongson2f_soft_off(void)
+{
+	unsigned int v0, a0;
+	//Power off value is 1
+	const unsigned char power_off_value = 1;
+
+#ifdef CONFIG_32BIT
+	#define AddrBase 0xbfd00000
+#else
+	#define AddrBase 0xffffffffbfd00000
+#endif
+	*(volatile unsigned char *)(AddrBase + SMBUS_HOST_DATA0) = power_off_value;
+	*(volatile unsigned char *)(AddrBase + SMBUS_HOST_DATA1) = 0;
+	
+	//ST7 chip addr 
+	*(volatile unsigned char *)(AddrBase + SMBUS_HOST_ADDRESS) = 0x24;
+	
+	//Offset 
+	*(volatile unsigned char *)(AddrBase + SMBUS_HOST_COMMAND) = 0x0;
+
+	//Byte Write
+	*(volatile unsigned char *)(AddrBase + SMBUS_HOST_CONTROL) = 0x08;
+
+	v0 = *(volatile unsigned char *)(AddrBase + SMBUS_HOST_STATUS);
+	v0 &= 0x1f;
+	if (v0)
+	{
+		*(volatile unsigned char *)(AddrBase + SMBUS_HOST_STATUS) = v0;
+		v0 = *(volatile unsigned char *)(AddrBase + SMBUS_HOST_STATUS);
+	}
+	
+	//Start
+	v0 = *(volatile unsigned char *)(AddrBase + SMBUS_HOST_CONTROL);
+	v0 |= 0x40;
+	*(volatile unsigned char *)(AddrBase + SMBUS_HOST_CONTROL) = v0;
+
+	do
+	{
+		//Wait
+		for (a0 = 0x1000; a0 != 0;);
+			a0--;
+		v0 = *(volatile unsigned char *)(AddrBase + SMBUS_HOST_STATUS);
+	}while(v0 & SMBUS_HOST_STATUS_BUSY);
+
+	v0 = *(volatile unsigned char *)(AddrBase + SMBUS_HOST_STATUS);
+	v0 &= 0x1f;
+	if (v0)
+	{
+		*(volatile unsigned char *)(AddrBase + SMBUS_HOST_STATUS) = v0;
+		v0 = *(volatile unsigned char *)(AddrBase + SMBUS_HOST_STATUS);
+	}
 }
