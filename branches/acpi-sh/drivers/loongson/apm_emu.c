2ecb3c5ced6a30ba59be63061a37868d0101afce
swadmin.os <swadmin.os@4d105e58-db3a-0410-9e81-b9488bac83f7>
2008-05-14 17:22:00 +0000

    modified for acpi develop

--- branches/acpi-sh/drivers/loongson/apm_emu.c
+++ branches/acpi-sh/drivers/loongson/apm_emu.c
@@ -129,8 +129,61 @@ static void ls_apm_get_power_status(struct apm_power_info *info)
 	
 }
 
+
+
+#define mipssaveregcp0(reg, value) asm volatile("li $8, 0\t\n" "mfc0 $8, $"#reg"\t\n" "sd $8,%0\t\n":"=m"(value));
+
+
 static int __init apm_emu_init(void)
 {
+	unsigned long cp0[32];
+	int i;
+
+	
+
+	mipssaveregcp0(0, cp0[0]);
+	mipssaveregcp0(1, cp0[1]);
+	mipssaveregcp0(2, cp0[2]);
+	mipssaveregcp0(3, cp0[3]);
+	mipssaveregcp0(4, cp0[4]);
+	mipssaveregcp0(5, cp0[5]);
+	mipssaveregcp0(6, cp0[6]);
+	mipssaveregcp0(7, cp0[7]);
+	mipssaveregcp0(8, cp0[8]);
+	mipssaveregcp0(9, cp0[9]);
+	mipssaveregcp0(10, cp0[10]);
+	mipssaveregcp0(11, cp0[11]);
+	mipssaveregcp0(12, cp0[12]);
+	mipssaveregcp0(13, cp0[13]);
+	mipssaveregcp0(14, cp0[14]);
+	mipssaveregcp0(15, cp0[15]);
+	mipssaveregcp0(16, cp0[16]);
+	mipssaveregcp0(17, cp0[17]);
+	mipssaveregcp0(18, cp0[18]);
+	mipssaveregcp0(19, cp0[19]);
+	mipssaveregcp0(20, cp0[20]);
+	mipssaveregcp0(21, cp0[21]);
+	mipssaveregcp0(22, cp0[22]);
+	mipssaveregcp0(23, cp0[23]);
+	mipssaveregcp0(24, cp0[24]);
+	mipssaveregcp0(25, cp0[25]);
+	mipssaveregcp0(26, cp0[26]);
+	mipssaveregcp0(27, cp0[27]);
+	mipssaveregcp0(28, cp0[28]);
+	mipssaveregcp0(29, cp0[29]);
+	mipssaveregcp0(30, cp0[30]);
+	mipssaveregcp0(31, cp0[31]);
+
+
+	for (i = 0; i < 32; i++)
+	  printk("cp0_%d = %x  ",i, cp0[i]);
+	
+	printk("\n");
+//	asm volatile(
+//		"mfc0 $8,$12\t\n"
+//		"sd $8, %0\t\n":"=m"(cp0)
+//	);
+
 
 	apm_get_power_status = ls_apm_get_power_status;	
  
