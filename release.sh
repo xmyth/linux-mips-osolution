92fb41cb5b89ce05d332b9ec7fc079194b8d8245
swadmin.os <swadmin.os@4d105e58-db3a-0410-9e81-b9488bac83f7>
2008-05-18 16:00:53 +0000

    add nore option for release.sh

--- release.sh
+++ release.sh
@@ -2,9 +2,13 @@
 echo
 
 CONFIG_NAME=ls2fdev
+CLEANALL=true
 if [ "$#" -eq 1 ] && [ "$1" == "xgi" ];then
 	echo "Build xgifb as video driver"
 	CONFIG_NAME=ls2fdev_xgi
+elif [ "$#" -eq 1 ] && [ "$1" == "nore" ]; then
+	echo "no clean"
+	CLEANALL=false
 else
 	echo "Build sisfb as video driver"
 fi
@@ -15,5 +19,8 @@ REV_NUM=${Revision##* }
 echo "Revision:$REV_NUM"
 echo
 
-make mrproper && make ${CONFIG_NAME}_defconfig && make && rm -fr rel && mkdir rel && INSTALL_MOD_PATH=rel make modules_install && mkdir rel/boot && cp vmlinux rel/boot/ && cd rel && tar -cjf ../linux-2.6.23.1-r$REV_NUM-${CONFIG_NAME}.tar.bz2 boot lib
+if $CLEANALL; then
+	make mrproper && make ${CONFIG_NAME}_defconfig
+fi
+make && rm -fr rel && mkdir rel && INSTALL_MOD_PATH=rel make modules_install && mkdir rel/boot && cp vmlinux rel/boot/ && cd rel && tar -cjf ../linux-2.6.23.1-r$REV_NUM-${CONFIG_NAME}.tar.bz2 boot lib
 
