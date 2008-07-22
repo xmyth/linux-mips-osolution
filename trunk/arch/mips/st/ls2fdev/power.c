fad6cf0a58f11204cdc07d45d6f69b19c756adb2
swadmin.os <swadmin.os@4d105e58-db3a-0410-9e81-b9488bac83f7>
2008-07-22 09:02:47 +0000

    remove some debug info

--- trunk/arch/mips/st/ls2fdev/power.c
+++ trunk/arch/mips/st/ls2fdev/power.c
@@ -71,50 +71,50 @@ void restore_nb_registers(void) {
 	}
 }
 
-int acpi_sleep_prepare(void)
+int ls_sleep_prepare(void)
 {
 	__flush_cache_all();
 	local_flush_tlb_all();
 	return 0;
 }
 
-static struct pm_ops acpi_pm_ops;
+static struct pm_ops ls_pm_ops;
 
 
 extern void do_suspend_lowlevel(void);
 
 /**
- *	acpi_pm_set_target - Set the target system sleep state to the state
+ *	ls_pm_set_target - Set the target system sleep state to the state
  *		associated with given @pm_state, if supported.
  */
 
-static int acpi_pm_set_target(suspend_state_t pm_state)
+static int ls_pm_set_target(suspend_state_t pm_state)
 {
-	printk("acpi_pm_set_target\n");
+	printk("ls_pm_set_target\n");
 	return 0;
 }
 
 /**
- *	acpi_pm_prepare - Do preliminary suspend work.
+ *	ls_pm_prepare - Do preliminary suspend work.
  *	@pm_state: ignored
  *
  *	If necessary, set the firmware waking vector and do arch-specific
  *	nastiness to get the wakeup code to the waking vector.
  */
 
-static int acpi_pm_prepare(suspend_state_t pm_state)
+static int ls_pm_prepare(suspend_state_t pm_state)
 {
-	acpi_sleep_prepare();
-	printk("acpi_pm_prepare\n");
+	ls_sleep_prepare();
+	printk("ls_pm_prepare\n");
 	return 0;
 }
 
 /**
- *	acpi_pm_enter - Actually enter a sleep state.
+ *	ls_pm_enter - Actually enter a sleep state.
  *	@pm_state: ignored
  *
  *	Flush caches and go to sleep. For STR we have to call arch-specific
- *	assembly, which in turn call acpi_enter_sleep_state().
+ *	assembly, which in turn call ls_enter_sleep_state().
  *	It's unfortunate, but it works. Please fix if you're feeling frisky.
  */
 
@@ -124,13 +124,11 @@ extern void trap_init(void);
 extern int g_debug_dumpstack;
 extern int vga_bios_post(void);
 
-static int acpi_pm_enter(suspend_state_t pm_state)
+static int ls_pm_enter(suspend_state_t pm_state)
 {
 
-//	unsigned long c,i;
 	struct pci_dev *pdev = NULL;
 	unsigned long flags = 0;
-	//printk("acpi_pm_enter\n");
 	
 	save_nb_registers();
 
@@ -140,39 +138,6 @@ static int acpi_pm_enter(suspend_state_t pm_state)
 
 	do_suspend_lowlevel();
 
-
-	/*
-	
-	for (i = 0; i < 64; i += 4) {
-		pci_read_config_dword(pdev, i, (u32 *)&c);
-		printk("	%lx:%lx", i, c);
-		if ((i + 4 ) % 0x10 == 0)
-			printk("\n");
-	}
-
-	for (i = 0; i < 0x98; i += 4) {
-		if ( i % 0x10 == 0)
-			printk("\n");
-		printk("%2x:%8x ", (unsigned int)i, *(volatile unsigned int *) (0xffffffffbfe00100 + i));
-	}
-	printk("\n");
-
-	*(volatile unsigned int *)(0xffffffffbfe00100+0x10) = 0x2040;
-	*(volatile unsigned int *)(0xffffffffbfe00100+0x90) = 0x404040;
-	
-	for (i = 0; i < 0x98; i += 4) {
-		if ( i % 0x10 == 0)
-			printk("\n");
-		printk("%2x:%8x ", (unsigned int)i, *(volatile unsigned int *) (0xffffffffbfe00100 + i));
-	}
-	printk("\n");
-
-	printk("sizeof unsigned long = %d\n", (int)sizeof(unsigned long));
-
-
-
-	vga_bios_post();
-*/
 	local_flush_tlb_all();
 	restore_nb_registers();
 	
@@ -187,58 +152,30 @@ static int acpi_pm_enter(suspend_state_t pm_state)
 
 	pci_reenable_device(pdev);
 
-	BootVideoCardBIOS(pdev,  0);
+	BootVideoCardBIOS(pdev,  1);
 	local_irq_restore(flags);
-
-	//printk(KERN_DEBUG "Back to C!\n");
-	//printk(KERN_INFO "acpi_pm_enter exit\n");
-//	g_debug_dumpstack = 1;
 
 	return 0;
 }
 
 /**
- *	acpi_pm_finish - Finish up suspend sequence.
+ *	ls_pm_finish - Finish up suspend sequence.
  *	@pm_state: ignored
  *
  *	This is called after we wake back up (or if entering the sleep state
  *	failed). 
  */
 
-static int acpi_pm_finish(suspend_state_t pm_state)
+static int ls_pm_finish(suspend_state_t pm_state)
 {
-	struct pci_dev *pdev = NULL;
-
-	for_each_pci_dev(pdev) {
-		if ((pdev->class >> 8) == PCI_CLASS_DISPLAY_VGA) {
-			printk(KERN_INFO "pci->class = %x\n", pdev->class);
-			break;
-		}
-	}
-
-/*
-	//pci_restore_state(pdev);
-
-	//pci_reenable_device(pdev);
-	
-	for (i = 0; i < 64; i += 4) {
-		pci_read_config_dword(pdev, i, (u32 *)&c);
-		printk("	%lx:%lx", i, c);
-		if ((i + 4 ) % 0x10 == 0)
-			printk("\n");
-	}
-	vga_bios_post(pdev);
-*/
-	//BootVideoCardBIOS(pdev,  0);
-
-	printk("acpi_pm_finish\n");
+	printk("ls_pm_finish\n");
 	return 0;
 }
 
-static int acpi_pm_state_valid(suspend_state_t pm_state)
+static int ls_pm_state_valid(suspend_state_t pm_state)
 {
 
-	printk("acpi_pm_state_valid\n");
+	printk("ls_pm_state_valid\n");
 
 	switch (pm_state) {
 	case PM_SUSPEND_MEM:
@@ -248,31 +185,31 @@ static int acpi_pm_state_valid(suspend_state_t pm_state)
 	}
 }
 
-static struct pm_ops acpi_pm_ops = {
-	.valid = acpi_pm_state_valid,
-	.set_target = acpi_pm_set_target,
-	.prepare = acpi_pm_prepare,
-	.enter = acpi_pm_enter,
-	.finish = acpi_pm_finish,
+static struct pm_ops ls_pm_ops = {
+	.valid = ls_pm_state_valid,
+	.set_target = ls_pm_set_target,
+	.prepare = ls_pm_prepare,
+	.enter = ls_pm_enter,
+	.finish = ls_pm_finish,
 };
 
 
-static int __init acpi_sleep_init(void)
+static int __init ls_sleep_init(void)
 {
 
-	pm_set_ops(&acpi_pm_ops);
+	pm_set_ops(&ls_pm_ops);
 
-	printk("acpi_sleep_init done!\n");
+	printk("ls_sleep_init done!\n");
 	return 0;
 }
 
-static void __exit acpi_sleep_exit(void)
+static void __exit ls_sleep_exit(void)
 {
 }
 
 
-module_init(acpi_sleep_init);
-module_exit(acpi_sleep_exit);
+module_init(ls_sleep_init);
+module_exit(ls_sleep_exit);
 MODULE_AUTHOR("Aimeng");
 MODULE_DESCRIPTION("ACPI Module for Loongson 2E/2F Board");
 MODULE_LICENSE("GPL");
